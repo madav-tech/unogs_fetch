2ef91ba081a12b7203a667e4c16b5055f0317563
Nadav Daviv <denadav@gmail.com>
2020-10-16 17:12:33 +0300

    Prints list of all titles to json file (no parsing)

--- unogs_fetch.py
+++ unogs_fetch.py
@@ -1,8 +1,20 @@
 import requests
-import xml.etree.ElementTree as T
+from bs4 import BeautifulSoup as BS
+import json
 
-r = requests.get("http://unogs.com/search/?country_andorunique=or&start_year=1900&end_year=2020&end_rating=10&genrelist=&audiosubtitle_andor=or&countrylist=336")
-with open("response.html", 'w') as response_file:
-    response_file.write(r.text)
 
-tree = T.parse("response.html")
+query = {"limit": 6000, "offset": 0, "countrylist": 336, "country_andorunique": "or", "start_year": 1900, "end_year": 2020, "end_rating": 10, "audiosubtitle_andor": "or"}
+head =  {
+        'Referer': 'http://unogs.com/search/?country_andorunique=or&start_year=1900&end_year=2020&end_rating=10&genrelist=&audiosubtitle_andor=or&countrylist=336',
+        'REFERRER': 'http://unogs.com'
+        }
+
+r = requests.get("http://unogs.com/api/search", params = query, headers = head)
+
+results = r.json()
+
+with open("title_dump.json", 'w') as f:
+    f.write(json.dumps(results, indent = 2))
+
+# with open("name_list.txt", 'w') as f:
+#     for entry in results["results"]:
